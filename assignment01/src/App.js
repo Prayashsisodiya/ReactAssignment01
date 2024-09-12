fa73d5cc138ac7a0144c03272dc330f614f7b8e1
Prayash Sisodiya <prayashsisodiya36@gmail.com>
2024-09-12 22:32:43 +0530

    Question one of Assignment one is Complited Succesfully!!

---
+++ assignment01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import One from './Component/one';
+
+function App() {
+  return (
+    <div>
+      <One/>
+    </div>
+  )
+}
+
+export default App;
---
+++ assignment01/src/Component/one.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function One() {
+    const catchValue = ()=>{
+        let x = document.getElementById("hexCode");
+
+        let hexCode = x.value;
+        // console.log(hexCode)
+        let colorPreviewBox = document.getElementById("colorPreviewBox");
+        colorPreviewBox.style.backgroundColor = hexCode;
+    }
+  return (
+    <div>
+      <p>Welcome in  one.js </p>
+      <input type="text" name="colorBox" id="hexCode" placeholder='enter HEX color value' onChange={()=>catchValue()} />
+      {/* <button onClick={() => catchValue()}>Submit</button> */}
+
+      <div id="colorPreviewBox"></div>
+    </div>
+  )
+}
+
+// 1. Creating the Color Input Field: Implement an input field that accepts HEX color values (e.g., #FFFFFF for white). This will involve managing an input state that updates on every keystroke.
+
+// 2. Displaying the Color Preview: Below the input field, display a large, square div that serves as a preview of the color. The background color of this div should change in real-time as the user types in a HEX color value.
+
+// 3. User Feedback**: Ensure that the application provides feedback to the user if the entered HEX color code is invalid. This could be a simple text message indicating the error.
